bf110d4cf6a39dac4d006f98d945d2e0071dd45e
ChrisLasenski <81719725+Chris-Lasneski@users.noreply.github.com>
2025-05-16 16:37:18 -0700

    Deleted unneeded files

--- Final/app/src/main/java/com/example/final_project_team_32/ChatItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.final_project_team_32
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class ChatItem(
-    var message: String,
-    var owner: String,
-    var reaction: String) : java.io.Serializable
