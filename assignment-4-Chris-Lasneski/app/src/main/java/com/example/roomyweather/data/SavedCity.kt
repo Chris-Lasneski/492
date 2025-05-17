1bec7357c7821b7561f9af64c0cf0b42d35819db
ChrisLasenski <81719725+Chris-Lasneski@users.noreply.github.com>
2025-05-17 08:17:33 -0700

    Delete assignment-4-Chris-Lasneski directory

--- assignment-4-Chris-Lasneski/app/src/main/java/com/example/roomyweather/data/SavedCity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.roomyweather.data
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
-data class SavedCity(
-    @PrimaryKey val name: String,
-    val lastViewed: Long = System.currentTimeMillis())
